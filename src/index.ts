99063d28dde74ec218e9806f8fdaba1c1943ffbc
aquariuslt <superaquariuslt@gmail.com>
2020-02-24 15:59:00 +0800

    fix: remove patches for `@types/amqp-connection-manager`

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,3 @@ export * from './amqp.options';
 export * from './amqp.providers';
 export * from './amqp.module';
 
-// shared extensions
-export * from './shared/simple.abstract.producer';
-export * from './shared/simple.abstract.consumer';
