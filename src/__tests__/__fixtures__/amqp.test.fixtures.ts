0a40ae4d8db0f7fc0dfd25c02e52bbc86ab56626
aquariuslt <superaquariuslt@gmail.com>
2020-03-21 23:39:52 +0800

    test: update ci environment RMQ_URL injection

--- src/__tests__/__fixtures__/amqp.test.fixtures.ts
+++ src/__tests__/__fixtures__/amqp.test.fixtures.ts
@@ -1,1 +1,4 @@
-export const AMQP_TEST_URLS = ['amqp://devuser:devuser@localhost:5672']
+
+const AMQP_TEST_URL = process.env.CI ? process.env.RABBITMQ_URL : 'amqp://devuser:devuser@localhost:5672'
+
+export const AMQP_TEST_URLS = [AMQP_TEST_URL]
