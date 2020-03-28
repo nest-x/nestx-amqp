5366e70719397c62850335fd3cf6d60865784adf
aquariuslt <superaquariuslt@gmail.com>
2020-03-28 18:33:14 +0800

    chore: export `publish-exchange` and `exchange-producer`

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ export * from './amqp.providers'
 export * from './amqp.module'
 
 export * from './decorators/publish-queue'
+export * from './decorators/publish-exchange'
 export * from './decorators/subscribe-queue'
 export * from './decorators/inject-connection'
 
 export * from './interfaces/exchange'
 export * from './interfaces/queue'
 
-export * from './services/queueProducer'
+export * from './services/queue-producer'
+export * from './services/exchange-producer'
 export * from './services/consumer'
